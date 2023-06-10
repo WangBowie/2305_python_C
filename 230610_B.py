3b02d52d4f18cf5d9e92d8af2dab14cd87bb31a4
WangBowie <132725490+WangBowie@users.noreply.github.com>
2023-06-10 07:00:41 +0000

    230610

--- 230610_B.py
+++ 230610_B.py
@@ -5,4 +5,7 @@ codeFrame = pd.read_csv('codeSearch.csv',usecols=['code','name'])
 codeSeries = codeFrame['code'].astype(str) + codeFrame['name']
 
 with st.sidebar:
-    st.write("股票號碼：")
+    selected_codes = st.multiselect("請選擇股票:",codeSeries,
+                                    max_selections=4)
+
+st.write(selected_codes)
