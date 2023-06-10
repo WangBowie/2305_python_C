02dbb47adf5de0120db2614159196eae354b28ce
WangBowie <132725490+WangBowie@users.noreply.github.com>
2023-06-10 06:51:57 +0000

    230610

--- 230610_B.py
+++ 230610_B.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+
+codeFrame = pd.read_csv('codeSearch.csv',usecols=['code','name'])
+codeSeries = codeFrame['code'].astype(str) + codeFrame['name']
 
 with st.sidebar:
     st.write("股票號碼：")
